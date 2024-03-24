3d64cb63e70ac82ef7209a4a711e257b9480858b
doni-zete <donizete112@gmail.com>
2024-03-24 14:34:24 -0300

    feat: Iniciando o projeto livraria

---
+++ api-livraria-spring-boot/src/test/java/com/donizetesa/demo/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donizetesa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
